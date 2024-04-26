9e11ff78285cc0bd1c4f1ea0bd4dd739ee63addb
AFFISH <168150710+AFFISH@users.noreply.github.com>
2024-04-25 19:04:45 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,7 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome affisher and Family
-
-# header
+# Welcome affisher
 
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
