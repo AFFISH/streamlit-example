9adba67f55d74c0668903156959b6a2e70579fdd
AFFISH <168150710+AFFISH@users.noreply.github.com>
2024-04-25 19:47:12 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
 
-Jonathan is a goofball:
+the girls are goofballs:
 """
 
 num_points = st.slider("Number of points in spiral", 1,500, 100)
